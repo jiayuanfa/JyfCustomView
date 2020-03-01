5fca68db3b5309f7525843066165fb82fcc95a9a
jiayuanfa <jiayuanfa@yeah.net>
2020-03-02 04:47:10 +0800

    修改Utils通过Resources来获取资源

--- app/src/main/java/com/example/myapplication/TestView.java
+++ app/src/main/java/com/example/myapplication/TestView.java
@@ -57,6 +57,6 @@ public class TestView extends View {
          * 圆心x,y以及半径、画笔
          * 这里单位是px，但是我们绘制的时候，可以使用方法把dp转化使用
          */
-        canvas.drawCircle(getWidth() / 2f, getHeight() / 2f, Utils.dp2px(RADIUS, getContext()), paint);
+        canvas.drawCircle(getWidth() / 2f, getHeight() / 2f, Utils.dp2px(RADIUS), paint);
     }
 }
--- app/src/main/java/com/example/myapplication/Utils.java
+++ app/src/main/java/com/example/myapplication/Utils.java
@@ -1,8 +1,7 @@
 package com.example.myapplication;
 
-import android.content.Context;
+import android.content.res.Resources;
 import android.util.TypedValue;
-import android.view.View;
 
 /**
  * 工具类
@@ -12,7 +11,6 @@ public class Utils {
     /**
      * dp 转化为 px
      * @param dp
-     * @param context
      * 为什么需要一个参数为context ?
      * 通过getResources().getDisplayMetrics()方法我们得知，这句话的意思是跟显示相关的信息放在资源文件里面
      * 所以，这里也可以改成 Resources.system().getDisplayMetrics 获取跟系统资源相关的信息
@@ -20,7 +18,7 @@ public class Utils {
      * 资源信息包含什么？我们自定义的字符串、图片等，以及手机像素等都放在里面
      * @return
      */
-    public static float dp2px(float dp, Context context) {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+    public static float dp2px(float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, Resources.getSystem().getDisplayMetrics());
     }
 }
